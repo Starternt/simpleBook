a7eabd50ce17e679cff5c50672d4c72779868169
Administrator <Starternh@gmail.com>
2018-03-29 12:03:07 +0400

    Edited migration books table
    Fixed table name

--- database/migrations/2018_03_22_002017_create_books_table.php
+++ database/migrations/2018_03_22_002017_create_books_table.php
@@ -16,7 +16,7 @@ class CreateBooksTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->string('author');
-            $table->integer('published_year');
+            $table->integer('publish_year');
             $table->string('description');
             $table->string('image');
             $table->timestamps();
